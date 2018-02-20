eacb2bdc5077ea530a2ed6a5a0039ae2c17398a4
Joel Olsson <joel_1st@hotmail.com>
2018-02-20 17:43:49 +1000

    more styling niceness

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export class Create extends React.Component<{openNav: Function}, {}> {
   constructor(props: any) {
@@ -9,14 +9,10 @@ export class Create extends React.Component<{openNav: Function}, {}> {
   render() {
     return (
       <View style={styles.mainContent}>
-        <TouchableOpacity onPress={() => this.props.openNav()}>
-          <Text>Open Navbar</Text>
-        </TouchableOpacity>
-        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458'}}>
-          <Image source={require('../images/logo.png')} style={{ width: 58, height: 45, marginTop: 10, marginBottom: 10 }} />
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458', marginTop: 10, padding: 5}}>
-          <Text style={{color: 'white'}} >wutup</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#1b2835', height: 80}}>
+          <TouchableOpacity onPress={() => this.props.openNav()}>
+            <Text>Open Navbar</Text>
+          </TouchableOpacity>
         </View>
         <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 0, padding: 5}}>
           <Text style={{color: 'white'}} >heythere</Text>
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -5,14 +5,14 @@ export class Menu extends React.Component {
   render() {
     return (
       <View style={styles.menuContent}>
-        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458'}}>
-          <Image source={require('../images/logo.png')} style={{ width: 50, height: 40, marginTop: 10, marginBottom: 10 }} />
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458', padding: 10}}>
+          <Image source={require('../images/logo.png')} style={{ width: 48, height: 38, marginTop: 10, marginBottom: 10 }} />
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458', marginTop: 10, padding: 5}}>
-          <Text style={{color: 'white'}} >wutup</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', backgroundColor: '#2e4458', marginTop: 18, margin: 12, padding: 8, borderWidth: 1, borderColor: 'rgba(255, 255, 255, 0.2)'}}>
+          <Text style={{color: 'white', fontSize: 16}} >wutup</Text>
         </View>
-        <TouchableOpacity style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 0, padding: 5}}>
-          <Text>Add Wallet</Text>
+        <TouchableOpacity style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 0, borderWidth: 1, marginLeft: 12, marginRight: 12, padding: 8, borderColor: 'yellow'}}>
+          <Text style={{color: 'yellow'}}>Add Wallet</Text>
         </TouchableOpacity>
       </View>
     );
