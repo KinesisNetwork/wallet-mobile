a7d67e98edd41cdee5320b9d28c77ec52774f861
Joel Olsson <joel.olsson@bullioncapital.com>
2018-03-01 11:18:55 +1000

    Add password unlock for private key

--- src/Balances.tsx
+++ src/Balances.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native'
-import { getActiveWallet, getPrivateKey } from './helpers/wallets';
+import { Button, TextInput, Text, View } from 'react-native'
+import { getActiveWallet } from './helpers/wallets';
 import { BackNav } from './Navigation';
 import { AppState } from './Routing'
+import { decryptPrivateKey } from './services/encryption';
 let StellarSdk = require('stellar-sdk')
 
 export class Balances extends React.Component<{ screenProps: {appState: any}}, any> {
@@ -14,7 +15,7 @@ export class Balances extends React.Component<{ screenProps: {appState: any}}, a
 
   constructor (props: any) {
     super(props)
-    this.state = { account: null, kinesisBalance: 0, accountActivated: false, password: '' }
+    this.state = { account: null, kinesisBalance: 0, accountActivated: false, password: '', decryptedPrivateKey: '' }
   }
 
   componentDidMount() {
@@ -32,6 +33,19 @@ export class Balances extends React.Component<{ screenProps: {appState: any}}, a
     this.loadBalances(this.props)
   }
 
+  public async unlockWallet() {
+    let decryptedPrivateKey = decryptPrivateKey(getActiveWallet(this.props.screenProps.appState).encryptedPrivateKey, this.state.password)
+    if (decryptedPrivateKey) {
+      this.setState({decryptedPrivateKey})
+    } else {
+      console.warn('incorrect pass')
+    }
+  }
+
+  public handlePassword(password: any) {
+    this.setState({password})
+  }
+
   public async loadBalances(props: any) {
     try {
       const server = new StellarSdk.Server(props.screenProps.appState.connection.horizonServer, {allowHttp: true})
@@ -47,10 +61,12 @@ export class Balances extends React.Component<{ screenProps: {appState: any}}, a
 
   render() {
     return (
-      let activeWallet = getActiveWallet(this.props.screenProps.appState) || {}
       <BalancesPresentation
+        handlePassword={this.handlePassword.bind(this)}
+        unlockWallet={this.unlockWallet.bind(this)}
         appState={this.props.screenProps.appState}
-        privateKey={activeWallet}
+        privateKey={this.state.decryptedPrivateKey}
+        password={this.state.password}
         kinesisBalance={this.state.kinesisBalance}
         accountActivated={this.state.accountActivated}
       />
@@ -61,6 +77,10 @@ export class Balances extends React.Component<{ screenProps: {appState: any}}, a
 
 export class BalancesPresentation extends React.Component<{
   appState: AppState,
+  handlePassword: Function,
+  unlockWallet: Function,
+  password: string,
+  privateKey: string,
   kinesisBalance: number,
   accountActivated: boolean
 }, {}> {
@@ -77,11 +97,11 @@ export class BalancesPresentation extends React.Component<{
         <Text>Reveal Private Key: </Text>
 
         {(this.props.privateKey) ? (
-          <Text>{getPrivateKey(this.props.appState, activeWallet) || 'Please enter your wallet password'}</Text>
+          <Text>{this.props.privateKey || 'Please enter your wallet password'}</Text>
         ) : (
           <View>
-            <TextInput value={this.props.password} style={{backgroundColor: 'white', marginBottom: 15}} onChangeText={(text) => this.props.handleMemo(text)} />
-            <Button title='Transfer' onPress={() => this.props.viewPrivateKey()} />
+            <TextInput value={this.props.password} style={{backgroundColor: 'white', marginBottom: 15}} onChangeText={(text) => this.props.handlePassword(text)} />
+            <Button title='Unlock' onPress={() => this.props.unlockWallet()} />
           </View>
         )}
 
